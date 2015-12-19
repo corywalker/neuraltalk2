12f3311525ed150e3962888271113e9bb52b5365
Cory Walker <cwalker32@gmail.com>
2015-12-19 00:47:40 +0000

    Restart if crashed.

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-th myeval.lua -model model_id1-501-1448236541.t7 -image_folder /tmp/uploaded/ -dump_images 0
+while :; do
+    th myeval.lua -model model_id1-501-1448236541.t7 -image_folder /tmp/uploaded/ -dump_images 0;
+    sleep 1;
+done
